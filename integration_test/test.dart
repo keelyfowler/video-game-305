1f55ede30aff0a6280eaba7fbcd19268385a5390
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-14 21:12:04 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -63,7 +63,7 @@ void main() async {
         find.byKey(const ValueKey('Login-Password_n5z7')), '123456');
     await tester.tap(find.byKey(const ValueKey('Login-Button67')));
     await tester.pumpAndSettle(const Duration(milliseconds: 3000));
-    expect(find.byKey(const ValueKey('FeedPage_p0oj')), findsWidgets);
+    expect(find.byKey(const ValueKey('Text_qhut')), findsOneWidget);
   });
 }
 
